cdc15f0093127660ad5edbdaf3c9aa4888b0f9d5
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:50:01 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function TopBar() {
   return (
-    <div className='w-full flex items-center justify-start'>
+    <div className='w-full h-[55px] bg-orange-600 flex items-center justify-start'>
         {/* logo */}
         <div className='w-fit h-fit'>
             <img src="./logo_black.png" alt="logo" loading='lazy' className='h-[25px] w-fit' />

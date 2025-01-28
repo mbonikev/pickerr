2e536bb9be2a7890d78269ee0e76690d3e84301d
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 15:16:04 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ function Home() {
         <div className="w-full h-full bg-white rounded-2xl">
           <div className="border-b border-stone-200 flex items-center justify-center">
             {/* tabs */}
-            <button className="w-full py-3 px-6 text-sm"></button>
+            <button className="w-full py-3 px-6 text-sm font-medium">ss</button>
           </div>
         </div>
       </div>

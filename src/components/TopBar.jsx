53f2e39c94cf4db94375b6af47dfbbeed74ec0fd
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:23:29 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -5,7 +5,7 @@ function TopBar() {
     <div className='w-full flex items-center justify-start'>
         {/* logo */}
         <div className='w-fit miw-[120px]'>
-            <img src="./logo_black.png" alt="logo" loading='lazy' className='' />
+            <img src="./logo_black.png" alt="logo" loading='lazy' className='h-[20px]' />
         </div>
     </div>
   )

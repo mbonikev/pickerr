78f5b090b6dd45f1f56fda02026426e81750b40a
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:19:28 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,9 @@ function Home() {
         {/* content */}
         <div className='flex-1 min-h-full'></div>
         {/* tweaks */}
-        <div className='w-[32vw] max-w-[32vw] min-h-full bg-blue-500 p2'></div>
+        <div className='w-[32vw] max-w-[32vw] min-h-full bg-blue-500 p-2'>
+          <div className='w-full h-full bg-white'></div>
+        </div>
     </div>
   )
 }

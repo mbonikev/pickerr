adc3eb2a49fa27691e66c2c3c2c97e9e418cdf36
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:16:58 +0200

    updated src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
-import "./App.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import "./index.css";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const hasHash = window.location.hash.includes("#");
+  if (!hasHash && window.location.pathname !== "/") {
+    return <NotFound />;
+  }
   return (
-    <>
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </HashRouter>
-    </>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </HashRouter>
   );
 }
 

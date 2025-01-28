b5daab9e2752a408e4a645f609578ea99d18c6f3
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:21:08 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,8 +6,8 @@ function Home() {
       {/* content */}
       <div className="flex-1 min-h-full"></div>
       {/* tweaks */}
-      <div className="w-[32vw] max-w-[32vw] min-h-full bg-bodyColor p-2">
-        <div className="w-full h-full bg-white rounded-xl"></div>
+      <div className="w-[32vw] max-w-[32vw] min-h-full bg-bodyColor p-4">
+        <div className="w-full h-full bg-white rounded-2xl"></div>
       </div>
     </div>
   );

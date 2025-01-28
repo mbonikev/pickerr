33ca9d1820c07cb1205b379b06170b41b4a48fc6
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:54:52 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -11,7 +11,9 @@ function TopBar() {
         <div className='h-[20px] flex items-center justify-center bg-white px-2 rounded-xl text-sm font-medium text-textColorWeak'>V1.0</div>
         {/* links */}
         <div className='flex items-center justify-center px-3'>
-          <Link to="/"></Link>
+          <Link to="/" className='text-sm'>Report</Link>
+          <Link to="/" className='text-sm'></Link>
+          <Link to="/" className='text-sm'></Link>
         </div>
     </div>
   )

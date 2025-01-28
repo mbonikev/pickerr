0e1498b044bec8d1e1b18a6c3be7da78bb1ce42e
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:20:58 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div className='w-full h-svh flex bg-bodyColor'>
-        {/* content */}
-        <div className='flex-1 min-h-full'></div>
-        {/* tweaks */}
-        <div className='w-[32vw] max-w-[32vw] min-h-full bg-blue-500 p-2'>
-          <div className='w-full h-full bg-white rounded-xl'></div>
-        </div>
+    <div className="w-full h-svh flex bg-bodyColor">
+      {/* content */}
+      <div className="flex-1 min-h-full"></div>
+      {/* tweaks */}
+      <div className="w-[32vw] max-w-[32vw] min-h-full bg-bodyColor p-2">
+        <div className="w-full h-full bg-white rounded-xl"></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

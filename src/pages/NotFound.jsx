5404a50e1374d879f4d28b1cb6d627c0b18ad181
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:43:30 +0200

    updated src/pages/NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -10,7 +10,7 @@ function NotFound() {
       </p>
       <a
         href="/"
-        className="mt-4 px-4 py-2 bg-stone-800 text-white rounded-lg shadow hover:bg-mainColor/80 transition"
+        className="mt-4 px-4 py-2 bg-stone-800 text-white rounded-lg shadow"
       >
         Go Back Home
       </a>

cb4af68724880f7ee333f37498073ae9918b3b6a
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:22:08 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TopBar() {
   return (
-    <div className='w-full'>TopBar</div>
+    <div className='w-full flex items-center justify-start'>TopBar</div>
   )
 }
 

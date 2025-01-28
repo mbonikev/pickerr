4b7c5a1cea793763c30c675a5e527600a5192632
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 15:51:10 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,8 +30,8 @@ function Home() {
             </button>
           </div>
           <div className="h-full py-10 flex flex-col items-center justify-center gap-3 px-5">
-            <h1 className="text-xl font-bold">Import Excell File</h1>
-            <h1 className="text-xl font-bold">piccker will automatically detect first column, make sure your entries are in the correct comun</h1>
+            <h1 className="text-xl font-bold text-center">Import Excell File</h1>
+            <h1 className="text-base font-bold text-center">piccker will automatically detect first column, make sure your entries are in the correct comun</h1>
             <button
               className={`${tabStyle} max-w-[200px] border-transparent bg-textColor text-white rounded-xl `}
             >

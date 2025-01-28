4ed9d271ef7d694dbbcb15e767438aa3fe14ecb5
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 15:31:25 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import TopBar from "../components/TopBar";
 import { LuFileSpreadsheet, LuListMinus } from "react-icons/lu";
 
 function Home() {
-  const tabStyle = `w-full py-3 px-2 text-base font-medium flex items-center justify-center gap-1 border-b-[2px] outline-none cursor-pointer`;
+  const tabStyle = `w-full py-3 px-2 text-base font-medium flex items-center justify-center gap-1 border-b-[2px] outline-none cursor-pointer`
   return (
     <div className="w-full h-svh flex bg-bodyColor p-4 gap-4">
       {/* content */}
@@ -13,41 +13,22 @@ function Home() {
       {/* tweaks */}
       <div className="w-[32vw] max-w-[32vw] min-h-full bg-bodyColor">
         <div className="w-full h-full bg-white rounded-2xl">
-          <div className="w-full h-fit flex items-center justify-start gap-1.5 p-5">
-            {/* Logo */}
-            <div className="w-fit h-full">
-              <img
-                src="./logo_black.png"
-                alt="logo"
-                loading="lazy"
-                className="h-[25px] w-fit"
-              />
-            </div>
-            <div className="h-[20px] flex items-center justify-center bg-bodyColor px-2 rounded-xl text-sm font-medium text-textColorWeak">
-              V1.0
-            </div>
-          </div>
+          <h1 className="p-5 text-xl font-medium">Tweaks</h1>
           <div className="border-b border-stone-200 flex items-center justify-center gap-3 px-5">
             {/* tabs */}
             <button className={`${tabStyle} border-textColor `}>
               <LuListMinus className="text-lg" />
               Lists
             </button>
-            <button
-              className={`${tabStyle} text-textColorWeak border-transparent`}
-            >
+            <button className={`${tabStyle} text-textColorWeak border-transparent`}>
               <LuFileSpreadsheet className="text-lg" />
               Excell Sheet
             </button>
-            <button
-              className={`${tabStyle} text-textColorWeak border-transparent`}
-            >
+            <button className={`${tabStyle} text-textColorWeak border-transparent`}>
               <LuListMinus className="text-lg" />
               Lists
             </button>
-            <button
-              className={`${tabStyle} text-textColorWeak border-transparent`}
-            >
+            <button className={`${tabStyle} text-textColorWeak border-transparent`}>
               <LuListMinus className="text-lg" />
               Lists
             </button>

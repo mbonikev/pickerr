81aff4aa7c8e5122529d4fde432e4874ac1b893c
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:52:12 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -9,7 +9,7 @@ function TopBar() {
             <img src="./logo_black.png" alt="logo" loading='lazy' className='h-[25px] w-fit' />
         </div>
         {/* links */}
-        <Link className='h-[20px] flex items-center justify-center bg-yellow-500'>Picker</Link>
+        <div className='h-[20px] flex items-center justify-center bg-white'>Picker</div>
     </div>
   )
 }

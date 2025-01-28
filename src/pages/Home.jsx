a58acd3fc9fd859fe6840e14863cb6ecebce3171
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:21:48 +0200

    updated src/pages/Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import TopBar from "../components/TopBar";
 
 function Home() {
   return (
     <div className="w-full h-svh flex bg-bodyColor p-4 gap-4">
       {/* content */}
       <div className="flex-1 min-h-full">
-        
+        <TopBar />
       </div>
       {/* tweaks */}
       <div className="w-[32vw] max-w-[32vw] min-h-full bg-bodyColor">

1974abb9ad548227a158afc20b22671611bac236
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 12:09:25 +0200

    updated src/

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </HashRouter>
+    </>
+  );
+}
+
+export default App;

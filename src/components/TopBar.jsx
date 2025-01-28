7f3d6e06b16e8ddda32705e4bcb08a6eeb061576
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 15:04:03 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -22,7 +22,7 @@ function TopBar() {
         <button className="text-lg">
           <LuSettings />
         </button>
-        <Link to="/how-it-works" className="text-base px-3 bg-mainColor text-white py-0.5 text-sm rounded-lg">
+        <Link to="/how-it-works" className="text-base px-3 bg-mainColor text-white py-1 rounded-lg">
           Sign in
         </Link>
       </div>

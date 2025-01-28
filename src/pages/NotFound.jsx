ad5354455944ed793ae0c9557c27802b2939e317
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 14:44:41 +0200

    updated src/pages/NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function NotFound() {
   return (
     <div className="p-3 flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-5xl font-bold mb-1 text-stone-800">404</h1>
+      <h1 className="text-5xl font-bold mb-1 text-textColor">404</h1>
       <p className="text-xl mb-2">Page Not Found</p>
       <p className="text-textColorWeak mb-2">
         The page you're looking for doesn't exist or has been moved.
       </p>
       <a
         href="/"
-        className="mt-4 px-4 py-2 bg-stone-800 text-white rounded-lg shadow"
+        className="mt-4 px-4 py-2 bg-textColor text-white rounded-lg shadow"
       >
         Go Back Home
       </a>

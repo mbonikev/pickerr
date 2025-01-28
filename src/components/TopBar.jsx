29600db56c5191e2b7aea510f4a0dea4ee3b459a
mbonikev <mbonimpayekevin@gmail.com>
2025-01-28 15:03:03 +0200

    updated src/components/TopBar.jsx

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { LuSettings } from "react-icons/lu";
+import { Link } from "react-router-dom";
 
 function TopBar() {
   return (
     <div className="w-full h-[36px] flex items-center justify-start gap-1.5">
       {/* Logo */}
       <div className="w-fit h-full">
-        <img src="./logo_black.png" alt="logo" loading="lazy" className="h-[25px] w-fit" />
+        <img
+          src="./logo_black.png"
+          alt="logo"
+          loading="lazy"
+          className="h-[25px] w-fit"
+        />
       </div>
       <div className="h-[20px] flex items-center justify-center bg-white px-2 rounded-xl text-sm font-medium text-textColorWeak">
         V1.0
       </div>
       {/* Links */}
-      <div className="flex-1 flex items-center justify-end px-4 gap-6">
+      <div className="flex-1 flex items-center justify-end px-4 gap-3">
         <Link to="/history" className="text-base">
           History
         </Link>
-        <Link to="/saved-lists" className="text-base">
-          Saved Lists
-        </Link>
+        <button className="text-base">
+          <LuSettings />
+        </button>
         <Link to="/how-it-works" className="text-base">
           Sign in
         </Link>
